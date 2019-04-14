553519069f447716755551036a2d8410b9f25b67
guoyupeng <842553705@qq.com>
2019-04-14 20:36:11 +0800

    两种不同的方法，总体思路一样，先把数据放到map里面，根据映射取值

--- roman-to-integer/src/com/guoxiaohei/RomanToInteger.java
+++ roman-to-integer/src/com/guoxiaohei/RomanToInteger.java
@@ -17,6 +17,14 @@ public class RomanToInteger {
         System.out.println(romanToInteger("IX"));
         System.out.println(romanToInteger("LVIII"));
         System.out.println(romanToInteger("MCMXCIV"));
+        System.out.println(romanToInteger("MMCCCXCIX"));
+
+
+        System.out.println(romanToInteger2("IV"));
+        System.out.println(romanToInteger2("IX"));
+        System.out.println(romanToInteger2("LVIII"));
+        System.out.println(romanToInteger2("MCMXCIV"));
+        System.out.println(romanToInteger2("MMCCCXCIX"));
     }
 
 
@@ -30,22 +38,52 @@ public class RomanToInteger {
         maps.put("D", 500);
         maps.put("M", 1000);
         String[] romans = roman.split("");
+        int result = 0;
+        int len = romans.length;
+        for (int i = 0; i < len; i++) {
+            int value = maps.get(romans[i]);
+            if (i <= len - 2 && value < maps.get(romans[i + 1])) {
+                result = result - value;
+            } else {
+                result = result + value;
+            }
+        }
+        return result;
+    }
+
+    public static Integer romanToInteger2(String roman) {
+        Map<String, Integer> maps = new HashMap<>(7);
+        maps.put("I", 1);
+        maps.put("V", 5);
+        maps.put("X", 10);
+        maps.put("L", 50);
+        maps.put("C", 100);
+        maps.put("D", 500);
+        maps.put("M", 1000);
+        String[] romans = roman.split("");
         int result = maps.get(romans[0]);
         for (int i = 1; i < romans.length; i++) {
-            if ("V".equals(romans[i]) && "I".equals(romans[i - 1]) || "X".equals(romans[i]) && "I"
-                    .equals(romans[i - 1])) {
+            if ("V".equals(romans[i]) && "I".equals(romans[i - 1])) {
                 result = result - 1 * 2;
             }
-            if ("L".equals(romans[i]) && "X".equals(romans[i - 1]) || "C".equals(romans[i]) && "X"
-                    .equals(romans[i - 1])) {
+            if ("X".equals(romans[i]) && "I".equals(romans[i - 1])) {
+                result = result - 1 * 2;
+            }
+            if ("L".equals(romans[i]) && "X".equals(romans[i - 1])) {
                 result = result - 10 * 2;
             }
-            if ("D".equals(romans[i]) && "C".equals(romans[i - 1]) || "C".equals(romans[i]) && "X"
-                    .equals(romans[i - 1])) {
+            if ("C".equals(romans[i]) && "X".equals(romans[i - 1])) {
+                result = result - 10 * 2;
+            }
+            if ("D".equals(romans[i]) && "C".equals(romans[i - 1])) {
+                result = result - 100 * 2;
+            }
+            if ("M".equals(romans[i]) && "C".equals(romans[i - 1])) {
                 result = result - 100 * 2;
             }
             result += maps.get(romans[i]);
         }
         return result;
     }
+
 }
