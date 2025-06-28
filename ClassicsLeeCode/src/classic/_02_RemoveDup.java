cf4938ef64768e24e925015ba477b4ac6b15db95
guoxiaohei <842553705@qq.com>
2025-06-28 10:21:48 +0800

    leecode 1. add remove dup nums element

--- ClassicsLeeCode/src/classic/_02_RemoveDup.java
+++ ClassicsLeeCode/src/classic/_02_RemoveDup.java
@@ -1,24 +1,33 @@
 package classic;
 
+/**
+ * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+ * <p>
+ * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+ * <p>
+ * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+ * 返回 k 。
+ */
 public class _02_RemoveDup {
 
     /**
      * mermind flow 图
      * flowchart TD
-     *     subgraph Array1
-     *         A[元素1]
-     *         B[元素2]
-     *         C[元素3]
-     *     end
+     * subgraph Array1
+     * A[元素1]
+     * B[元素2]
+     * C[元素3]
+     * end
+     * <p>
+     * subgraph Array2
+     * D[元素A]
+     * E[元素B]
+     * F[元素C]
+     * end
+     * <p>
+     * B -- 比较 --> D
+     * C -- 比较 --> E
      *
-     *     subgraph Array2
-     *         D[元素A]
-     *         E[元素B]
-     *         F[元素C]
-     *     end
-     *
-     *     B -- 比较 --> D
-     *     C -- 比较 --> E
      * @param args
      */
     public static void main(String[] args) {
@@ -35,6 +44,18 @@ public class _02_RemoveDup {
                 nums[newArrayIndex] = nums[index];
             }
             index = index + 1;
+        }
+        return newArrayIndex + 1;
+    }
+
+
+    public static int removeDuplicates2(int[] nums) {
+        int newArrayIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[newArrayIndex]) {
+                newArrayIndex++;
+                nums[newArrayIndex] = nums[i];
+            }
         }
         return newArrayIndex + 1;
     }
---
+++ ClassicsLeeCode/src/classic/_02_RemoveDup2.java
@@ -0,0 +1,63 @@
+package classic;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ */
+public class _02_RemoveDup2 {
+
+
+    public static void main(String[] args) {
+//        int i = removeDuplicates(new int[]{1, 1, 1, 1});
+//        int i = removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 1, 1, 2});
+//        int i = removeDuplicates2(new int[]{1, 2});
+        int i = removeDuplicates2(new int[]{1});
+        System.out.println(i);
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int repeatCount = 0;
+        int newArrayIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[newArrayIndex]) {
+                newArrayIndex++;
+                nums[newArrayIndex] = nums[i];
+                repeatCount = 0;
+            } else {
+                repeatCount = repeatCount + 1;
+                if (repeatCount < 2) {
+                    newArrayIndex++;
+                    nums[newArrayIndex] = nums[i];
+                }
+            }
+        }
+        return newArrayIndex + 1;
+    }
+
+
+    /**
+     * 双指针解题法
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates2(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int newArrayIndex = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[newArrayIndex - 2]) {
+                nums[newArrayIndex++] = nums[i];
+            }
+        }
+        return newArrayIndex;
+    }
+}
