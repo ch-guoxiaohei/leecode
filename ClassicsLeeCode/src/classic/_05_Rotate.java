f970aec11d41098934c6e49cb8213af46ba72e31
guoxiaohei <842553705@qq.com>
2025-06-30 18:45:59 +0800

    leecode rotate three times rotate

--- ClassicsLeeCode/src/classic/_05_Rotate.java
+++ ClassicsLeeCode/src/classic/_05_Rotate.java
@@ -11,11 +11,30 @@ import java.util.Arrays;
 public class _05_Rotate {
 
     public static void main(String[] args) {
-        rotate(new int[]{1, 2}, 1);
+        int[] ints = {1, 2};
+        rotate(ints, 3);
+        System.out.println(Arrays.toString(ints));
     }
 
 
     public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        // 处理k大于数组长度的情况 ,当k=n时属于无效rotate，因为轮转之后仍然是相同的顺序
+        int maxRotateSize = k % n;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, maxRotateSize - 1);
+        reverse(nums, maxRotateSize, nums.length - 1);
+    }
 
+
+    public static void reverse(int[] nums, int start, int end) {
+
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
