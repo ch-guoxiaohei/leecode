f4170f0da91747dfe6d6c81c305744da16bc12c5
guoxiaohei <842553705@qq.com>
2025-06-30 17:31:24 +0800

    leecode 1. majority Element search in num arrays

---
+++ ClassicsLeeCode/src/classic/_05_Rotate.java
@@ -0,0 +1,21 @@
+package classic;
+
+
+import java.util.Arrays;
+
+/**
+ * 轮转数组
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * <a href="https://leetcode.cn/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150">轮转数组</a>
+ */
+public class _05_Rotate {
+
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2}, 1);
+    }
+
+
+    public static void rotate(int[] nums, int k) {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# leecode
+# 算法 leecode 练习
+
+## [ClassicsLeeCode](ClassicsLeeCode) 存放`leecode`的解题方法。
+
+### 常见的解题技巧
+
+- 双指针解题法
+- 投票算法 | Boyer-Moore BM算法
+- 三次反转法
+- 环状替换法
